f60c57d91eafe833fb6e461534b69ca62d2a77fe
k92020806009 <k92020806009@KOMP409-11>
2018-01-13 12:05:02 +0100

    drugi

---
+++ src/Shop.Web/Framework/MyMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Shop.Web.Framework
+{
+    public class MyMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public MyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)//asynchronicznie bo czekamy na odpowiedz serwera jakiegos. a nie chcemy zzerac zasobow. ale gdy zasoby procesora uzywane- matematyczne to to nic nie da. tylko do zewnetrznych.
+        {
+            Console.WriteLine($"My middleware: {httpContext.Request.Path}");
+            await _next.Invoke(httpContext);
+        }
+
+
+    }
+}
